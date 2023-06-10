1fe10bb63279269f7c91349604af28a583480b69
Brad Leege <bleege@gmail.com>
2023-06-10 16:16:30 -0500

    Changing shared content

--- shared/src/commonMain/kotlin/io/leege/hellomultiplatformworld/Greeting.kt
+++ shared/src/commonMain/kotlin/io/leege/hellomultiplatformworld/Greeting.kt
@@ -4,6 +4,6 @@ class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        return "Hello, guess what the name is: ${platform.name.reversed()}"
     }
 }
