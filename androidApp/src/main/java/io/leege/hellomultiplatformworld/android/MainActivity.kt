6be54261de98d399ce950fdcda728ae676cc33d9
Brad Leege <bleege@gmail.com>
2023-06-10 23:42:07 -0500

    Make network data request via shared Ktor based functionality.  Android app updated to work with it.

--- androidApp/src/main/java/io/leege/hellomultiplatformworld/android/MainActivity.kt
+++ androidApp/src/main/java/io/leege/hellomultiplatformworld/android/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.leege.hellomultiplatformworld.Greeting
@@ -19,7 +24,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    // https://dev.to/zachklipp/remember-mutablestateof-a-cheat-sheet-10ma
+                    var text by remember { mutableStateOf("Loading") }
+                    LaunchedEffect(true) {
+                        text = try {
+                            Greeting().greet()
+                        } catch (e: Exception) {
+                            e.localizedMessage ?: "error"
+                        }
+                    }
+                    GreetingView(text)
                 }
             }
         }
